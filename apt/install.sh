925c9e96dfe3f9e802756eb4cdf3b1f30a1bf7a1
Manuel Carrizo <manuelcarrizo@users.noreply.github.com>
2015-07-20 11:42:18 -0300

    Update install.sh

--- apt/install.sh
+++ apt/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CURRENT_PATH=`dirname ${BASH_SOURCE}`
-sudo apt-get install $(grep -vE "^\s*#" $CURRENT_PATH/packages  | tr "\n" " ")
+sudo apt-get install -y $(grep -vE "^\s*#" $CURRENT_PATH/packages  | tr "\n" " ")
 
 NEW_GROUPS="wireshark docker"
 for g in $NEW_GROUPS ; do sudo usermod -a -G $g $USER ; done
