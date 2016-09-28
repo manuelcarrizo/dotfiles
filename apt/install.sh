40e74dc38e06f76bbbec933699489e828be9183d
Manuel Carrizo <manuel.carrizo@avenida.com>
2016-09-28 17:02:00 -0300

    Added Debian support. Added strongswan VPN

--- apt/install.sh
+++ apt/install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+if [ "$SYSTEM" == "Debian" ]
+then
+    EXTRA_PKGS="$CURRENT_PATH/debian"
+elif [ "$SYSTEM" == "Ubuntu" ]
+then
+    EXTRA_PKGS="$CURRENT_PATH/ubuntu"
+fi
+
 CURRENT_PATH=`dirname ${BASH_SOURCE}`
-sudo apt-get install -y $(grep -vE "^\s*#" $CURRENT_PATH/packages  | tr "\n" " ")
+sudo apt-get install -y $(cat $CURRENT_PATH/packages $EXTRA_PKGS | grep -vE "^\s*#" | tr "\n" " ")
+
+curl -sSL https://get.docker.com/ | sh
 
 NEW_GROUPS="wireshark docker"
 for g in $NEW_GROUPS ; do sudo usermod -a -G $g $USER ; done
--- setup.sh
+++ setup.sh
@@ -2,15 +2,22 @@
 INSTALL_PATH=$HOME/.dotfiles
 BASHRC=$HOME/.bashrc
 
-# Use main repositories
-sudo sed -i.bkp 's/http:\/\/[a-z][a-z][.]/http:\/\//g' /etc/apt/sources.list
+# Get system information
+export SYSTEM=$(lsb_release -is)
+export RELEASE=$(lsb_release -cs)
+
+# Use main repositories on Xbuntu instead of regionals
+if [ "$SYSTEM" == "Ubuntu" ]
+then
+  sudo sed -i.bkp 's/http:\/\/[a-z][a-z][.]/http:\/\//g' /etc/apt/sources.list
+fi
 
 # install git
 sudo apt-get update
 sudo apt-get install -y git
 
 # clone repo
-if [ ! -d INSTALL_PATH ] ;
+if [ ! -d $INSTALL_PATH ] ;
 then
     echo "Cloning dotfiles to $INSTALL_PATH"
     git clone https://github.com/manuelcarrizo/dotfiles.git $INSTALL_PATH
